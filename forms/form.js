7e9524b9dafe51ccd05ff770a35b808379483e2a
Devika P S <devu1011depu@gmail.com>
2023-07-18 20:41:57 +0530

    background image added

--- forms/form.js
+++ forms/form.js
@@ -1,18 +1,70 @@
+$(document).ready(function () {
+  $("#submit-form").submit(function (e) {
+    e.preventDefault();
 
-$("#submit-form").submit((e)=>{
-  e.preventDefault()
+    // Get the form data
+    var formData = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+      subject: $("#subject").val(),
+      message: $("#message").val(),
+    };
+
+    // Validate name
+    if (formData.name == "") {
+      alert("Please enter a name");
+      return;
+    }
+
+    // Validate name length
+    if (formData.name.length < 3) {
+      alert("Name should be at least 3 characters long.");
+      return;
+    }
+
+    // Validate name only contains alphabets and dots
+    if (!/^[a-zA-Z.\s]+$/.test(formData.name)) {
+      alert("Name must contain only Alphabets and dots.");
+      return;
+    }
+
+    // Simple email format validation
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(formData.email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+
+    // Validate subject
+    if (formData.subject == "") {
+      alert("Please enter a subject");
+      return;
+    }
+
+    // Validate message
+    if (formData.message == "") {
+      alert("Please enter a message");
+      return;
+    }
+
+    // Submit the form
+    console.log(formData);
+    submit(formData);
+    $("#submit-form")[0].reset();
+  });
+});
+
+function submit(data) {
   $.ajax({
-      url:"https://script.google.com/macros/s/AKfycbxrr_RtGB8Zk4SMtVzLOszTbx-5ZWZ_432g2u8qDqSfnWgqth4XKAoJu0vGqFndQS3Q/exec",
-      data:$("#submit-form").serialize(),
-      method:"post",
-      success:function (response){
-          alert("Form submitted successfully")
-          // window.location.reload()
-          window.location.href="index.html"
-      },
-      error:function (err){
-          alert("Something Error")
-
-      }
-  })
-})
+    url: "https://script.google.com/macros/s/AKfycbw97-5GmZrlDcslN2EYz14XY_1B7IynKZMoCxZ2F90gF1Mh9RBNcAxBtOfCR5nCyWDh/exec",
+    data: data,
+    method: "post",
+    success: function (response) {
+      alert("Form submitted successfully");
+      window.location.reload();
+    },
+    error: function (err) {
+      alert("Something Error");
+    },
+  });
+}
